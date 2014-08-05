109c8767190d70831b6146eee4461853fc4ad5f7
Jorge Salgado Molinet <satowan@gmail.com>
2014-08-05 17:17:44 -0400

    function _decode OK
    Solución al error preg_replace(): The /e modifier is deprecated, use preg_replace_callback instead... al actualizar version de PHP para Joomla 1.7.

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-<?php
-
-	$x = "15" + 15;
-	echo $x;
-
-?>
+/**
+	 * Try to convert to plaintext
+	 *
+	 * @access	protected
+	 * @param	string	$source
+	 * @return	string	Plaintext string
+	 * @since	1.5
+	 */
+	function _decode($source)
+	{
+		// entity decode
+		$trans_tbl = get_html_translation_table(HTML_ENTITIES);
+		foreach($trans_tbl as $k => $v) {
+			$ttr[$v] = utf8_encode($k);
+		}
+		$source = strtr($source, $ttr);
+		// convert decimal
+		$source = preg_replace('/&#(\d+);/m', "utf8_encode(chr(\\1))", $source); // decimal notation
+		// convert hex
+		$source = preg_replace('/&#x([a-f0-9]+);/mi', "utf8_encode(chr(0x\\1))", $source); // hex notation
+		return $source;
+	}
